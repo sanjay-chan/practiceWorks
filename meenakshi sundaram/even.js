b00f7d313c99d998242db1d353936b7e36f1fc18
sundarammeenakshi <134413480+sundarammeenakshi@users.noreply.github.com>
2023-12-09 16:16:14 +0630

    second

--- meenakshi sundaram/even.js
+++ meenakshi sundaram/even.js
@@ -12,3 +12,32 @@ function even(array){
 }
 
 console.log(even(array));
+
+let array1=[625,763,66,736,44,23,22,37];
+
+function odd(array1){
+    let output=array1.filter(x=>x%2===0)
+
+    console.log(output)
+}
+
+console.log(odd(array1));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
