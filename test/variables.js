d625b4be51aecf5c59cd4655cca4541aff9ac434
subbu10136 <33937989+subbu10136@users.noreply.github.com>
2023-12-11 07:38:12 +0530

    variables

--- test/variables.js
+++ test/variables.js
@@ -1,2 +1,32 @@
  // file extenstiions
- 
+
+ console.log("testing")
+
+ let a = 3;
+ let x = "fw";
+ let h = ["gwheg","ejejhed","hdejkr"];
+ let z = [5,6,7,8];
+ let y = {name:"test", age:3};
+ let b = [
+    {name:"test", age:3},
+    {name:"testhh", age:7}
+ ]
+
+ let students = [
+    {name:"test", age:4, class:"7th"},
+    {name:"raju", age:10, class:"8th"}
+ ]
+
+// 
+console.log(" x length =  ", x.length)
+
+
+
+//console.log(" R = ", a)
+ a = 4;
+ //console.log("res =n" , a)
+
+
+console.log("res =n" , 2*a)
+
+console.log("res =n" , 8*a)
