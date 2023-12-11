247c7c3062b9358f8fc6b004e9a6f9f69357287f
gangavishnu12 <153348514+gangavishnu12@users.noreply.github.com>
2023-12-11 14:59:47 +0530

    push

---
+++ maxnumber.js
@@ -0,0 +1,10 @@
+//find maximum value//
+
+function maxnumber(numbers) {
+   // numbers = [1, 2, 3, 4, 5, 6, 7]
+   // console.log(numbers)
+    let output = Math.max(1, 2, 3, 4, 5, 6, 7);
+    return output;
+}
+let c = maxnumber(1, 2, 3, 4, 5, 6, 7)
+console.log("maximum number is=", c)
