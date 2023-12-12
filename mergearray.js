8070b52adf70b6c46c27affcb5c3cbec27f19a20
gangavishnu12 <153348514+gangavishnu12@users.noreply.github.com>
2023-12-12 09:37:34 +0530

    push

---
+++ mergearray.js
@@ -0,0 +1,11 @@
+function merge(arr1,arr2){
+     arr1=[1,2]
+     arr2=[6,7]
+      let arr3=arr1.concat(arr2)
+       return arr3;
+}
+let c=merge(1,2)
+    c=merge(6,7)
+    console.log("The merge value is=",c)
+
+    
