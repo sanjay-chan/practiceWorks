0d8b116e150a2cccc78346e0ed77f33dff35bc1c
Dhayanithi304 <dhayanithi1330@gmail.com>
2024-05-14 23:10:03 +0530

    modified

--- Dhayanithi/Drag/my-app/src/App.jsx
+++ Dhayanithi/Drag/my-app/src/App.jsx
@@ -1,17 +1,10 @@
 import React from "react";
-import Drag from "./Drag";
-import {ErrorBoundary} from "react-error-boundary";
 import Form from "./Form";
 import { ErrorBoundary } from "react-error-boundary";
 import FallBack from "./FallBack";
 
 function App() {
   const name = "name";
-
-  // const handleError = (error, errorInfo) => {
-  //   console.log(error);
-  //   console.log(errorInfo);
-  // }
   return (
     <ErrorBoundary FallbackComponent={FallBack}>
       <Form name={name} />
